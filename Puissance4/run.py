cbbdceb6db2020481daf65d7d1299a9f1c062563
Mathieu RODRIGUES <74418584+MathieuRodri@users.noreply.github.com>
2022-05-19 08:01:14 +0200

    Add difficulty + mode selection

--- Puissance4/run.py
+++ Puissance4/run.py
@@ -2,6 +2,15 @@ from game import *
 from miniMax import *
 
 
-Game().startIA()  # Min Max est nickel
-# Game().startIAAlphaBeta()#alpha beta block au dernier moment il sort de la board
-# Game().startMCT()
+def start():
+    gameMode = input('GameMode: (1-Human vs IA, 2-Human vs Human)')
+
+    if gameMode == '1':
+        Game().startIA()  # Min Max est nickel
+        # Game().startIAAlphaBeta()#alpha beta block au dernier moment il sort de la board
+        # Game().startMCT()
+    elif gameMode == '2':
+        Game().start()
+
+
+start()
